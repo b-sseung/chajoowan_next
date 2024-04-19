4d1e26aa17ad4c09e4b67b8079c4824ade7d544f
Bae SeungHee <66785642+b-sseung@users.noreply.github.com>
2024-04-19 11:54:10 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,6 @@ const nextConfig = {
   assetPrefix: process.env.NODE_ENV === 'production' ? 'https://b-seung.github.io/commision-0003' : '', // production 일때 prefix 경로
   trailingSlash: true, // 빌드 시 폴더 구조 그대로 생성하도록
   images: { unoptimized: true },
-  output: 'export',
 };
 
 export default nextConfig;
